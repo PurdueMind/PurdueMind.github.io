029f63d45faaeaf81ac2946fcfb3ac6788b9e0d0
Grant Congdon <grantdcongdon@gmail.com>
2026-01-17 16:57:17 -0500

    Fixing changes

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,6 +1,6 @@
 import './HomePage.css';
 import '../../App.css';
-import ToDisplay from '../../assets/home/shirt-design-flyer.png';
+//import ToDisplay from '../../assets/home/shirt-design-flyer.png';
 
 export default function HomePage() {
   return (
@@ -9,8 +9,6 @@ export default function HomePage() {
       <h1>Purdue MIND</h1>
 
       <p>Purdue MIND is a multidisciplinary student organization that promotes and grows the biomedical engineering community by providing undergraduates with hands-on experience in medical technology innovation—from research and design to commercialization—while fostering collaboration, professional networking, and participation in national design competitions.</p>
-
-      <h2><a href="https://forms.gle/AW5sP5hvR4kH7YYG9" target="_blank" rel="noopener noreferrer">Click Here for Submission Form!</a></h2>
 
       <h2><a href="https://join.slack.com/t/purdue-mind-workspace/shared_invite/zt-3cy3ljpxd-RqL5fJwChYdRKniiasXepw" target="_blank" rel="noopener noreferrer">Join our Slack!</a></h2>
 
@@ -35,4 +33,5 @@ export default function HomePage() {
           style={{ width: '80%', maxWidth: 900, height: 'auto', border: 'solid 1px #777' }}
         />
       </div>
+<h2><a href="https://forms.gle/AW5sP5hvR4kH7YYG9" target="_blank" rel="noopener noreferrer">Click Here for Submission Form!</a></h2>
 */
