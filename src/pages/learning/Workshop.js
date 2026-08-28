410e0209dec9304bdcb6cace1058a956aa780dc1
Grant Congdon <grantdcongdon@gmail.com>
2026-08-28 11:38:19 -0400

    Updated onboarding bypass link

--- src/pages/learning/Workshop.js
+++ src/pages/learning/Workshop.js
@@ -29,7 +29,7 @@ export default function Workshop({id, pitch, dates, buttonName, description, img
                 <h3 className='workshopPitch'><b>{pitch}</b></h3>
                 <h5 className='workshopDates'>{dates}</h5>
                 <DownloadButton downloadFile={downloadFile} filename={downloadFilename} buttonName={buttonName} />
-                <button className='downloadButton' onClick={() => {}}>I know what I'm doing</button>
+                <button className='downloadButton' onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSe1qXo_KUtuJfKfdbtoN5jIdKV0ntQNn9gHOpjK-QqUIl50tw/viewform', '_blank')}>I know what I'm doing</button>
             </div>
         </div>
     )
